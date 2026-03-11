00495813e43ba6ab52ed2ca7496125b49dc9a4b2
ZingerLittleBee <6970999@gmail.com>
2026-03-11 22:15:39 +0800

    init: new project

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,21 @@
+import { db } from '@serverbee/db'
+import * as schema from '@serverbee/db/schema/auth'
+import { env } from '@serverbee/env/server'
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'sqlite',
+
+    schema
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [tanstackStartCookies()]
+})
