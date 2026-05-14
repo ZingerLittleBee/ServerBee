3f01e2fddab9f13482547aa5dfda94b6cc2f456f
ZingerLittleBee <6970999@gmail.com>
2026-05-15 00:30:38 +0800

    test(web): add ResizeObserver polyfill for jsdom

--- apps/web/src/test/setup.ts
+++ apps/web/src/test/setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom'
+
+class ResizeObserverMock {
+  observe(): void {}
+  unobserve(): void {}
+  disconnect(): void {}
+}
+
+if (typeof globalThis.ResizeObserver === 'undefined') {
+  // @ts-expect-error jsdom does not implement ResizeObserver
+  globalThis.ResizeObserver = ResizeObserverMock
+}
