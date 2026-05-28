0673801b482007f8b51046ad652f10f173df5ad0
ZingerLittleBee <6970999@gmail.com>
2026-05-28 20:27:59 +0800

    feat(widget-sdk): domain hooks (alerts/services/traffic/uptime/history/geoip)

--- packages/widget-sdk/src/hooks/index.ts
+++ packages/widget-sdk/src/hooks/index.ts
@@ -1,2 +1,15 @@
+export {
+  type AlertEvent,
+  type HistoryPoint,
+  type ServiceMonitor,
+  type TrafficPoint,
+  type UptimeEntry,
+  useAlerts,
+  useGeoIp,
+  useHistory,
+  useServiceMonitors,
+  useTraffic,
+  useUptime
+} from './domain'
 export { useApiMutation, useApiQuery } from './escape-hatch'
 export { useCapability, useMetric, useServer, useServers } from './live'
